03dfed303e5ca062a23f035ceeb13217e03286e1
JangGwon <tlsakch510@naver.com>
2022-01-03 22:05:31 +0900

    algo

---
+++ back_1157.cpp
@@ -0,0 +1,2 @@
+//s#include <iostream>
+
---
+++ back_1546.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     int num;
+//     float sum;
+//     int max = 0;
+//     int array[1000];   
+
+//     scanf("%d",&num);
+//     for(int i = 0; i<num;i++)
+//     {   
+//          scanf("%d",&array[i]);
+//          if(max < array[i]) max = array[i];
+//     }       
+
+//     for(int i = 0; i <num; i++)
+//     {
+//         sum += array[i] / (float)max;
+//     }
+//     printf("%f",sum/(float)num*100);
+
+//     return 0;
+// }
