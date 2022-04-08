4d9bd1d175ea795bdad65653ce75638e81e8f9c5
JangGwon <tlsakch510@naver.com>
2022-04-08 14:43:43 +0900

    2022/04/08

--- back_2579.cpp
+++ back_2579.cpp
@@ -7,6 +7,9 @@ int dp[301] = {0,};
 
 int main()
 {
+    ios::sync_with_stdio(false);
+	cin.tie(0);
+
     int a;
     int v[301];
     cin >> a;
@@ -17,13 +20,14 @@ int main()
         v[i] = t;
     }
     dp[1] = v[1];
-    dp[2] = v[2];
-    dp[3] = v[3];
+    dp[2] = max(v[1] + v[2], v[2]);
+    dp[3] = max(v[1] + v[3], v[2] + v[3]);
 
     for (int i = 4; i <= a; i++)
     {
         dp[i] = max(dp[i-3] + dp[i-1], dp[i-2]) + v[i]; 
     }
+    cout << dp[a-1];
     return 0;
 
 }
