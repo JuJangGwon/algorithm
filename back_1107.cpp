45dd07510f05e90ebdc0ba07d2124f3a2b061ee0
JangGwon <tlsakch510@naver.com>
2022-04-16 23:59:35 +0900

    2022/04/16

--- back_1107.cpp
+++ back_1107.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+bool none[10];
+
+int made(int num)
+{
+    string c = to_string(num);
+    int i = 0;
+    for (int i = 0 ; i < c.size(); i++)
+    {
+        i*=10;
+        for (int j = c) 
+    }
+    return 0;
+}
+
 int main()
 {
+    int sum = 0;
+    int now = 100;
+    int a,b;
+    cin >> a >> b;
+    for (int i = 0 ; i < b; i++)
+    {
+        int c;
+        cin >> c;
+        none[c] = true;
+    }
+    while (now != a)
+    {
+        cout << "**";
+    }
+    cout << sum;
     return 0;
 }
