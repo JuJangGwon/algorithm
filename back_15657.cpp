5f7eaa717f9a5c99470538825dca8a9736d17100
JangGwon <tlsakch510@naver.com>
2022-04-18 00:55:05 +0900

    2022/04/18

---
+++ back_15657.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int result[9];
+int num[9];
+int a, b;
+
+void dfs(int c, int d)
+{
+    if (d == b)
+    {
+        for (int i = 0; i < b; i++)
+        {
+            cout << result[i] << " ";
+        }
+        cout << "\n";
+        return ;
+    }
+    else
+    {
+        for (int i = c; i < a; i++)
+        {
+            result[d] = num[i];
+            dfs(i+1, d+1);
+        }
+    }
+}
+
+int main()
+{
+    cin >> a >> b;
+    for (int i = 0; i < a; i++)
+    {
+        cin >> num[i];
+    }
+    sort(num,num+a);
+    dfs(0,0);
+}
