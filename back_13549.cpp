073e9e487eb0133f469670f2df19ca4f7c448a32
JangGwon <tlsakch510@naver.com>
2022-04-30 10:58:02 +0900

    2022/04/30

--- back_13549.cpp
+++ back_13549.cpp
@@ -10,27 +10,31 @@ bool used[100001];
 void bfs()
 {
     queue<pair<int, int> >q;
-    q.push(make_pair(a,0));
+    q.push(make_pair(0,a));
     while (!q.empty())
     {
-        int x = q.front().first;
-        int y = q.front().second;
+        int x = q.front().second;
+        int y = q.front().first;
         q.pop();
-
-        if (x == b)
+        if (x == 17)
         {
             cout << y;
-            break;
+            return ;
         }
-        if (!used[x-1] && x-1 >= 0)
+        if (x-1>= 0 && !used[x-1])
         {
             used[x-1] = true;
-            q.push(make_pair(x-1,y+1));
+            q.push(make_pair(y+ 1,x-1));
         }
-        if (!used[x+1] && x+1 <= 100001)
+        if (x+1 <= 100000 && !used[x+1])
         {
             used[x+1] = true;
-            q.push(make_pair(x-1,y+1));
+            q.push(make_pair(y+1,x+1));
+        }
+        if (x*2 <= 100000 && !used[x*2])
+        {
+            used[x*2] = true;
+            q.push(make_pair(y,x*2));
         }
     }
 }
@@ -39,4 +43,5 @@ int main()
 {
     cin >> a >> b;
     bfs();
+    return 0;
 }
