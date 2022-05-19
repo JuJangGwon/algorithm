e9d978ef298d1aa4140b45376f16a079e2e6e12c
JangGwon <tlsakch510@naver.com>
2022-05-20 00:46:35 +0900

    2022/05/20

--- back_2638.cpp
+++ back_2638.cpp
@@ -1,32 +1,100 @@
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
-int a, b;
+int a, b, c = 1;
 int map[101][101];
+int tmap[101][101];
+bool bmap[101][101];
 
-int main()
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+void find()
 {
-    cin >> a >> b;
-    // for (int i = 1; i <= a; i++)
-    // {
-    //     for (int j = 1; j <= b; j++)
-    //     {
-    //         cin >> map[i][j];
-    //     }
-    // }
-    map[4][4] = 1;
-    map[4][5] = 2;
-    map[3][4] = 1;
-    map[5][5] = 1;
-    map[5][6] = 1;
+    queue<pair<int, int> > q;
+    q.push(make_pair(1,1));
+    bmap[1][1] = true;
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++)
+        {
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if (yy >= 1 && xx >=1 && xx <= b && yy <= a && !bmap[yy][xx])
+            {
+                if (map[yy][xx] == 1)
+                {
+                    c++;
+                    tmap[yy][xx]--;
+                }
+                else
+                {
+                    bmap[yy][xx] = true;
+                    q.push(make_pair(xx,yy));
+                }
+            }
+        }
+    }
+}
+void clear()
+{
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1 ; j <= b ; j++)
+        {
+            tmap[i][j] = 0;
+            bmap[i][j] = 0;
+        }
+    }
+}
+void mi()
+{
     for (int i = 1; i <= a; i++)
     {
         for (int j =1 ; j<= b; j++)
         {
-            cout << map[i][j] << " ";
+            if (tmap[i][j] <= -2)
+            {
+                map[i][j] = 0;
+            }
         }
-        cout << "\n";
     }
+}
+int main()
+{
+    int d = 0;
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= b; j++)
+        {
+            cin >> map[i][j];
+        }
+    }
+    while(c != 0)
+    {
+        c = 0;
+        clear();
+        find();
+        if (c == 0)
+        {
+            cout << d;
+            break;
+        }
+        mi();
+        d++;
+    }
+    // for (int i = 1; i <= a; i++)
+    // {
+    //     for (int j =1 ; j<= b; j++)
+    //     {
+    //         cout << map[i][j] << " ";
+    //     }
+    //     cout << "\n";
+    // }
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,68 +1,8 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-
-using namespace std;
-int a;
-string str[12];
-int b[12];
-bool c[12];
-bool ov[28];
-int oo[28];
-int mm;
-
-void make()
-{
-    int ss = 0;
-    for (int i = 1 ; i<= a; i++)
+  for (int i = 1; i <= a; i++)
     {
-        int sum = 0;
-        for (int j = 0; j < str[i].size(); j++)
+        for (int j =1 ; j<= b; j++)
         {
-            sum *= 10;
-            sum += b[oo[str[i][j]-'A']];
+            cout << map[i][j] << " ";
         }
-        ss += sum;
+        cout << "\n";
     }
-    mm = max(mm,ss);
-}
-
-void dfs(int num)
-{
-    if (num == 10)
-    {
-        make();
-        return ;
-    }
-    for (int i = 0; i < 10; i++)
-    {
-        if (c[i] == false)
-        {
-            b[num] = i;
-            c[b[num]] = true;
-            dfs(num+1);
-            c[b[num]] = false;
-        }
-    }
-}
-
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cin >> a;
-    int nu = 0;
-    for (int i = 1; i <= a; i++)
-    {
-        cin >> str[i];
-        for (int j = 0; j < str[i].size(); j++)
-        {
-            if (!oo[str[i][j] - 'A'])
-            {
-                oo[str[i][j] - 'A'] = ++nu;
-            }
-        }
-    }
-    dfs(0);
-    cout << mm;
-}
