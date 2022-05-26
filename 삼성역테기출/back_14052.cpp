b80cfe857dc947a67c16d3cb100b155902390c43
JangGwon <tlsakch510@naver.com>
2022-05-27 02:01:36 +0900

    2022/05/27

---
+++ 삼성역테기출/back_14052.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int a, b;
+int map[9][9];
+int tempmap[9][9];
+int maxx = 0;
+bool used[9][9];
+vector<pair<int,int> >virus;
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+
+
+void clear()
+{
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j =1 ;j <=b; j++)
+        {
+            tempmap[i][j] = map[i][j];
+          //  cout << tempmap[i][j] << " ";
+            used[i][j] = false;
+        }
+    }
+}
+int check()
+{
+    int sum = 0;
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j =1 ; j<= b; j++)
+        {
+            if (tempmap[i][j] == 0)
+            {
+                sum++;
+            }
+        }
+    }
+    return sum;
+}
+
+void bfs()
+{
+    clear();
+
+    queue<pair<int,int> >q;
+    for(int i = 0; i < virus.size(); i++)
+    {
+        q.push(make_pair(virus[i].first,virus[i].second));
+        used[virus[i].second][virus[i].first] = true;
+    }
+    while (!q.empty())
+    {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i< 4; i++)
+        {
+            int xx = x + dx[i];
+            int yy = y + dy[i];
+            if (xx >=1 && xx <= b && yy >= 1 && yy <= a && !used[yy][xx] && tempmap[yy][xx] == 0)
+            {
+                tempmap[yy][xx] = 2;
+                used[yy][xx] = 1;
+                q.push(make_pair(xx,yy));
+            }
+        }
+    }
+}
+
+
+void dfs(int now,int x, int y)
+{
+    if (now == 3)
+    {
+        bfs();
+        maxx = max(maxx,check());
+        return ;
+    }
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= b; j++)
+        {
+            if (map[i][j] == 0)
+            {
+                map[i][j] = 1;
+                dfs(now+1,j,i);
+                map[i][j] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    cin >> a >> b;
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j =1 ;j <= b; j++)
+        {
+            cin >> map[i][j];
+            if (map[i][j] == 2)
+                virus.push_back(make_pair(j,i));    
+        }
+    }
+    dfs(0,1,1);
+    cout << maxx;
+    return 0;
+}
---
+++ 삼성역테기출/tempCodeRunnerFile.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <deque>
+#include <queue>
+
+using namespace std;
+
+queue<pair <int, char> > xc;
+deque<pair<int, int> >dq;
+
+int map[102][102]; // 1 사과 // 2 뱀 
+int dx[4] = {0,0,1,-1};
+int dy[4] = {1,-1,0,0};
+
+int dir = 1; // 1 right 2 down 3 left 4 up
+int m;
+int ct = 1;
+
+int main()
+{
+    cin >> m;
+    int n;
+    cin >> n;
+    for (int i = 1; i<=n ;i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        map[a][b] = 1;
+    }
+    int nn;
+    cin >> nn;
+    for (int i = 1; i<= nn;i++)
+    {
+        int a;
+        char b;
+        cin >> a >> b;
+        xc.push(make_pair(a,b));
+    }
+    dq.push_back(make_pair(1,1));
+    while (1)
+    {
+        int player_x = dq.front().first;
+        int player_y = dq.front().second;
+        //cout << player_x << ", " << player_y << " size : " << dq.size() << endl;
+        // 커브
+        if (ct == xc.front().first)
+        {
+            if (xc.front().second == 'D') // 왼
+            {
+                dir--;
+                if (dir == 0)
+                    dir = 4;
+            } 
+            else if (xc.front().second == 'L') // 오
+            {
+                dir++;
+                if (dir == 5)
+                {
+                    dir = 1;
+                }
+            }
+            xc.pop();
+        }
+        // 이동
+        if (dir == 1)
+        {
+            dq.push_front(make_pair(player_x+1,player_y));
+        }
+        else if (dir == 2)
+        {
+            dq.push_front(make_pair(player_x,player_y-1));
+        }
+        else if (dir == 3)
+        {
+            dq.push_front(make_pair(player_x-1,player_y));
+        }
+        else if (dir == 4)
+        {
+            dq.push_front(make_pair(player_x,player_y+1));
+        }
+        ct++;
+        // 끝나는지 안끝나는지 체크
+        if (map[dq.front().second][dq.front().first] == 2)
+        {
+            break ;
+        }
+        if (dq.front().second == 0 || dq.front().second == m+1 || dq.front().first == 0 || dq.front().first == m+1)
+        {
+            break; 
+        }
+        // 꼬리지울지 안지울지 체크
+        if (map[dq.front().second][dq.front().first] == 0)      // 0일때 
+        {
+            map[dq.back().second][dq.back().first] = 0;
+            dq.pop_back();
+        }
+        map[dq.front().second][dq.front().first] = 2;        
+    }
+    cout << ct;
+}
