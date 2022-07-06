11974c4ef62cc1cf502f2e693307c31aa4a1fad8
JangGwon <tlsakch510@naver.com>
2022-07-06 23:31:41 +0900

    2022/07/06

--- back_9466.cpp
+++ back_9466.cpp
@@ -1,34 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <memory.h>
 
 using namespace std;
 
 int t,a,b;
-int map[100001];
-bool bmap[100001];
 
+int map[100001];
+int bmap[100001];
+int counts = 0;
 void dfs(int i)
 {
-    
+    bmap[i]++;
+    if (bmap[i] == 3)
+    {
+        return ;
+    }
+    else if (bmap[i] <= 2)
+    {
+        dfs(map[i]);
+    }
+    if (bmap[i] < 2)
+    {
+        bmap[i] = 0;
+        return ;
+    }
 }
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
     cin >> t;
     for (int tt = 1; tt<= t; tt++)
     {
+        counts = 0;
+        memset(map,0,sizeof(map));
+        memset(bmap,0,sizeof(bmap));
         cin >> a;
         for (int i = 1; i<= a; i++)
         {
-            cin >> map[i];
+            int o;
+            cin >> o;
+            map[i]=o;
         }
         for (int i = 1; i <= a; i++)
         {
-            if (!map[i])
+            if (!bmap[i])
             {
-                bmap[map[i]] = true;
-                dfs(map[i]);
+                dfs(i);
+            }
+        }     
+        for (int i = 1; i <= a;i++)
+        {
+            if (bmap[i] == 0)
+            {
+                counts++;
             }
         }
+        cout << counts << "\n";
     }
-    
+
 }
