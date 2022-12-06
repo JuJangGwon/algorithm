cd4389503aaad617ae319875053e89dc4dbe98a0
JangGwon <tlsakch510@naver.com>
2022-12-06 14:40:10 +0900

    2022/12/06

---
+++ back_2444.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    
+    cin >> a;
+
+    for (int i =0; i < a;i++)
+    {
+        for (int j =i; j < a-1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i*2 +1; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    for (int i =a-2; i >= 0;i--)
+    {
+        for (int j =i; j < a-1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < i*2 +1; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
