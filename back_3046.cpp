09439324c21178bf958692a84f7eb8abfe0e6c57
JangGwon <tlsakch510@naver.com>
2022-12-10 18:03:01 +0900

    2022/12/10

---
+++ back_3046.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+	int n, avg;
+	int result;
+ 
+	cin >> n >> avg;
+ 
+	result = (avg * 2) - n;
+ 
+	cout << result << endl;
+ 
+	return 0;
+}
