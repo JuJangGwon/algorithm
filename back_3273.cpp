717cc2f9bd3efce2d7523427f2b98c5cc5f10507
JangGwon <tlsakch510@naver.com>
2022-12-18 19:24:24 +0900

    2022/12/18

---
+++ back_3273.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int map[100001];
+int n, m, result;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+    for (int i =0; i < n; i++)
+    {
+        cin >> map[i];
+    }
+    cin >> m;
+    sort(map,map+n);
+
+    int pivot1=0;
+    int pivot2= n-1;
+
+    while(pivot1 < pivot2)
+    {
+        if (map[pivot1] + map[pivot2] == m)
+        {
+            pivot1++;
+            pivot2--;
+            result++;
+        }
+        else if(map[pivot1] + map[pivot2] > m)
+        {
+            pivot2--;
+        }
+            else if(map[pivot1] + map[pivot2] < m)
+        {
+            pivot1++;
+        }
+    }
+    cout << result;
+}
