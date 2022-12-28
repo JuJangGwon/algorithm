adc93f8c621ec2ee32ceb4bd7594b7ff9a743458
JangGwon <tlsakch510@naver.com>
2022-12-28 22:05:22 +0900

    2022/12/28

---
+++ back_5800.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int k, n, c;
+
+bool cmp(int a, int b)
+{
+    return a > b;
+}
+int main() {
+    cin >> k;
+    while (k--)
+    {
+        int lg = 0;
+        cin >> n;
+        int *a = new int[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        sort(a,a+n,cmp);
+        for (int i = 0; i < n - 1; i++)
+            if (lg < a[i] - a[i+1])
+                lg = a[i] - a[i+1];
+        cout << "Class " << ++c<<'\n';
+        cout << "Max " << a[0] << ", Min " << a[n-1] << ", Largest gap " << lg <<'\n';
+    }
+}
