2b2fb0a7363aa0971b0cdebc4b0d656666b18952
JangGwon <tlsakch510@naver.com>
2023-04-19 11:54:59 +0900

    2023/04/19

---
+++ 프로그래머스/LV2_숫자의표현.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int n)
+{
+    int answer = 0, start = 1;
+
+    while (start <= n)
+    {
+        int tmp = 0;
+
+        for (int i = start; tmp <= n; i++)
+        {
+            if (tmp == n)
+                answer++;
+            tmp += i;
+        }
+        start++;
+    }
+    return answer;
+}
