f97b94753d9f80b8badfcaed8ddb9c66313e3e15
JangGwon <tlsakch510@naver.com>
2023-05-07 12:56:12 +0900

    2023/05/07

---
+++ 프로그래머스/LV1_실패율.cpp
@@ -0,0 +1,44 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+bool cmp(pair<double, int> a, pair<double, int> b)
+{
+    if (a.first == b.first)
+        return a.second < b.second;
+    else
+        return a.first > b.first;
+}
+
+vector<int> solution(int N, vector<int> stages)
+{
+    vector<int> answer;
+    vector<pair<double, int> > per;
+    map<int, double> ma;
+    int size = stages.size();
+
+    for (int i = 0; i < stages.size(); i++)
+    {
+        ma[stages[i]]++;
+    }
+    double minus_mem = 0;
+    for (int i = 1; i <= N; i++)
+    {
+        if (ma[i] == 0)
+        {
+            per.push_back({0, i});
+            continue;
+        }
+        per.push_back({ma[i] / (size - minus_mem), i});
+        minus_mem += ma[i];
+    }
+    sort(per.begin(), per.end(), cmp);
+    for (auto o : per)
+    {
+        answer.push_back(o.second);
+    }
+
+    return answer;
+}
