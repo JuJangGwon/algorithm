35b13551059098815a5ee021b83c52609950eacf
JangGwon <tlsakch510@naver.com>
2023-05-11 21:59:44 +0900

    2023/05/11

---
+++ back_1009.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+    for (int i = 1; i <= T; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        int c = a;
+        for (int i = 1; i < b; i++)
+        {
+            c *= a;
+            c %= 10;
+        }
+        if (c == 0)
+            cout << 10 << endl;
+        else
+            cout << c << endl;
+    }
+}
