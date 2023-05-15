9906e72b20911c1d1ce4e984a005ea05bfbf85ec
JangGwon <tlsakch510@naver.com>
2023-05-15 23:57:53 +0900

    2023/05/15

--- back_1759.cpp
+++ back_1759.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-char ccc[16];
+char input[16];
 char result[16];
 int l, c;
 bool used[16];
@@ -20,15 +20,15 @@ void dfs(int a, int b,int mo, int ja)
     }
     for (int i = a; i < c; i++)
     {
-        if (!used[ccc[i]])
+        if (!used[input[i]])
         {
-            result[b] = ccc[i];
-            used[ccc[i]] = true;
-            if (ccc[i] == 'a' || ccc[i] == 'e' || ccc[i] == 'i' || ccc[i] == 'u' || ccc[i] == 'o')
+            result[b] = input[i];
+            used[input[i]] = true;
+            if (input[i] == 'a' || input[i] == 'e' || input[i] == 'i' || input[i] == 'u' || input[i] == 'o')
                 dfs(i,b + 1,mo + 1,ja);
             else
                 dfs(i,b+1,mo,ja +1);
-            used[ccc[i]] = false;
+            used[input[i]] = false;
         }
     }
 }
@@ -38,8 +38,8 @@ int main()
     cin >> l >> c;
     for (int i  = 0; i < c; i++)
     {
-        cin >> ccc[i];
+        cin >> input[i];
     }
-    sort(ccc,ccc+c);
+    sort(input,input+c);
     dfs(0,0,0,0);
 }
