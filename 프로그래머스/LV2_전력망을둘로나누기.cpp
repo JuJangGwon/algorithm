8bb5dabbf0d96fc2262d2d380283081cd65c79df
JangGwon <tlsakch510@naver.com>
2024-05-18 21:33:25 +0900

    2024/05/18

--- 프로그래머스/LV2_전력망을둘로나누기.cpp
+++ 프로그래머스/LV2_전력망을둘로나누기.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int parent[101];
 
-int findpa(int a)                               // 부모찾기
+int findpa(int a)                              
 {
     if (parent[a] == a) return a;
     return parent[a] = findpa(parent[a]);
 }
-void unionpa(int a, int b)                      // 결합
+void unionpa(int a, int b)                      
 {
     a = findpa(a);
     b = findpa(b);
@@ -23,21 +23,21 @@ void unionpa(int a, int b)                      // 결합
 
 int solution(int n, vector<vector<int> > wires) {
     int answer = 101;
-    int temp = 0;                               // 끊을 송전탑 
+    int temp = 0;                              
     
     for (int i = 0; i < wires.size(); i++)
     {
-        for (int j = 1; j <= n; j++)            // 노드 초기화 
+        for (int j = 1; j <= n; j++)          
         {
             parent[j] = j;
         }
         for (int j = 0; j < wires.size(); j++) 
         {
-            if (j == temp)                      // 끊을 송전탑은 union 제외하기 
+            if (j == temp)                      
                 continue;
             unionpa(wires[j][0],wires[j][1]);
         }
-        int t = 0;                              // 갯수 세기용 임시 변수
+        int t = 0;                            
         for (int j = 1; j <= n; j++)
         {
             parent[j] = findpa(j);
