6fb8114f678242c59fe19f2516d63950e52353f9
unknown <tlsakch510@naver.com>
2025-04-21 23:27:53 +0900

    2025/04/21

---
+++ 자바스크립트/프로그래머스/LV2_테이블해시함수.js
@@ -0,0 +1,30 @@
+function solution(data, col, row_begin, row_end) {
+    var answer = 0;
+      col--;
+      data.sort((a,b)=>{
+        if(a[col]>b[col]){
+          return 1;
+        }
+        else if(a[col] === b[col]){
+          if(a[0]<b[0]){
+            return 1;
+          }
+          else{
+            return -1;
+          }
+        }
+        else{
+          return -1;
+        }
+      })
+    
+      for(var i = row_begin;i<=row_end;i++){
+        var sum = 0;
+        data[i-1].forEach(element => {
+          sum+=(element%i);
+        });
+        answer = answer ^ sum;
+  
+      }
+    return answer;
+  }
